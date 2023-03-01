5f8c06bec8569104494ad2862a3e615460e8dc2f
darshit akbari <darshitakbari9999@gmail.com>
2023-03-01 09:43:35 +0530

    d

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addCart } from '../redux/action/action';
 
 const Cart = () => {
+    
+    const product = useSelector((state) => state.handleCart);
 
-    const handleButton = () =>{
-        
-    }
+    useEffect(() => {
+        if(product) {
+            console.log("hello");
+        }
+      }, [product])
+     const dispatch = useDispatch();
+     const addProduct = (product) => {
+       dispatch(addCart(product[0]));
+       
+     }
     return (
         <div>
             <div>
                 <div className='row'>
                     <div className='col-md-4'>
-                        <img src={product.image} alt={product.title} height="200px" width="180px" />
+                        <img src={product[0].image} alt={product[0].title} height="200px" width="180px" />
                     </div>
                     <div className='col-md-4'>
-                        <h3>{product.title}</h3>
+                        <h3>{product[0].title}</h3>
                         <p className='lead fw-bold'>
-                            {product.qty} X ${product.price} = $
-                            {product.qty * product.price}
+                            {product[0].qty} X ${product[0].price} = $
+                            {product[0].qty * product[0].price}
                         </p>
-                        <button className='btn btn-outline-dark me-4' onClick={() => handleButton(product)}><i className='fa fa-minus'></i></button>
-                        <button className='btn btn-outline-dark me-4' onClick={() => handleButton(product)}><i className='fa fa-plus'></i></button>
+                        <button className='btn btn-outline-dark me-4' onClick={()=>addProduct(product[0])}><i className='fa fa-minus'></i></button>
+                        <button className='btn btn-outline-dark me-4' ><i className='fa fa-plus'></i></button>
                     </div>
                 </div>
             </div>
--- src/components/Prodects.jsx
+++ src/components/Prodects.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 import { useState, useEffect } from 'react';
 import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import 'react-loading-skeleton/dist/skeleton.css'
 import { NavLink } from 'react-router-dom';
 
 const Prodects = () => {
